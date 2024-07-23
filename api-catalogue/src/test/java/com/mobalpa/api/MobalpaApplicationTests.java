cbb9bdd65082800a77ff911eba97bb4aa3594709
Tanguy Gibrat <gibratanguy@icloud.com>
2024-07-23 10:11:23 +0200

    refactor: API for catalogue & delivery

---
+++ api-catalogue/src/test/java/com/mobalpa/api/MobalpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobalpa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobalpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ api-delivery/src/main/java/com/mobalpa/api/delivery/MobalpaApplication.java
@@ -0,0 +1,13 @@
+package com.mobalpa.api.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobalpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobalpaApplication.class, args);
+	}
+
+}
