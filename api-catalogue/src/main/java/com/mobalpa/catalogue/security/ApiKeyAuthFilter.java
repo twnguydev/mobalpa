c02c6f21e2dc7274fbbaa6fd0af3f284f928f7b2
Tanguy Gibrat <gibratanguy@icloud.com>
2024-07-29 12:10:54 +0200

    fix: security token in API Catalogue

--- api-catalogue/src/main/java/com/mobalpa/catalogue/security/ApiKeyAuthFilter.java
+++ api-catalogue/src/main/java/com/mobalpa/catalogue/security/ApiKeyAuthFilter.java
@@ -1,7 +1,14 @@
 package com.mobalpa.catalogue.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,10 +17,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 @Order(1)
 public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiKeyAuthFilter.class);
 
     @Value("${api.key}")
     private String apiKey;
@@ -23,12 +33,21 @@ public class ApiKeyAuthFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         String requestApiKey = request.getHeader("X-API-KEY");
+        logger.info("Received API Key: {}", requestApiKey);
+        logger.info("Configured API Key: {}", apiKey);
 
         if (apiKey.equals(requestApiKey)) {
+            logger.info("API Key is valid.");
+
+            UserDetails userDetails = User.withUsername("apiKeyUser").password("").authorities(Collections.emptyList()).build();
+            ApiKeyAuthenticationToken authentication = new ApiKeyAuthenticationToken(userDetails, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
             filterChain.doFilter(request, response);
         } else {
+            logger.warn("API Key is invalid.");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("Unauthorized");
+            response.getWriter().write("Unauthorized: Invalid API Key");
         }
     }
 }
