0ee5ae6718982ce7fda695beac55d8190d613068
Tanguy Gibrat <gibratanguy@icloud.com>
2024-07-30 16:17:18 +0200

    fix(front): auth/order services and interfaces

--- front/src/services/order.service.ts
+++ front/src/services/order.service.ts
@@ -4,58 +4,56 @@ import { Observable } from 'rxjs';
 import { IOrder } from '@interfaces/order.interface';
 import { IPayment } from '@interfaces/payment.interface';
 import { IWishlist, IWishlistItem } from '@interfaces/wishlist.interface';
-import { AuthService } from './auth.service';
+import { AuthService } from '@services/auth.service';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  private orderUrl: string = 'http://localhost:8081/api/orders';
-  private paymentUrl: string = 'http://localhost:8081/api/payments';
-  private wishlistUrl: string = 'http://localhost:8081/api/wishlist';
+  private orderUrl: string = `${environment.apiUrl}/orders`;
+  private paymentUrl: string = `${environment.apiUrl}/payments`;
+  private wishlistUrl: string = `${environment.apiUrl}/wishlist`;
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
-  // Order related methods
   createOrder(order: Omit<IOrder, 'orderId'>): Observable<IOrder> {
     const headers = this.authService.getAuthHeaders();
     return this.http.post<IOrder>(this.orderUrl, order, { headers });
   }
 
   getOrders(): Observable<IOrder[]> {
-    const headers = this.authService.getAuthHeaders();
+    const headers: HttpHeaders = this.authService.getAuthHeaders();
     return this.http.get<IOrder[]>(this.orderUrl, { headers });
   }
 
   getOrderById(orderId: string): Observable<IOrder> {
-    const headers = this.authService.getAuthHeaders();
+    const headers: HttpHeaders = this.authService.getAuthHeaders();
     return this.http.get<IOrder>(`${this.orderUrl}/${orderId}`, { headers });
   }
 
-  // Payment related methods
   getPayments(): Observable<IPayment[]> {
-    const headers = this.authService.getAuthHeaders();
+    const headers: HttpHeaders = this.authService.getAuthHeaders();
     return this.http.get<IPayment[]>(this.paymentUrl, { headers });
   }
 
   createPayment(payment: IPayment): Observable<IPayment> {
-    const headers = this.authService.getAuthHeaders();
+    const headers: HttpHeaders = this.authService.getAuthHeaders();
     return this.http.post<IPayment>(this.paymentUrl, payment, { headers });
   }
 
-  // Wishlist related methods
   getWishlist(): Observable<IWishlist> {
-    const headers = this.authService.getAuthHeaders();
+    const headers: HttpHeaders = this.authService.getAuthHeaders();
     return this.http.get<IWishlist>(this.wishlistUrl, { headers });
   }
 
   addToWishlist(item: IWishlistItem): Observable<IWishlist> {
-    const headers = this.authService.getAuthHeaders();
+    const headers: HttpHeaders = this.authService.getAuthHeaders();
     return this.http.post<IWishlist>(this.wishlistUrl, item, { headers });
   }
 
   removeFromWishlist(itemId: string): Observable<IWishlist> {
-    const headers = this.authService.getAuthHeaders();
+    const headers: HttpHeaders = this.authService.getAuthHeaders();
     return this.http.delete<IWishlist>(`${this.wishlistUrl}/${itemId}`, { headers });
   }
 }
