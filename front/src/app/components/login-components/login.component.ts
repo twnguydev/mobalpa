d978ad95efb802808b3e79f04d4ccefcfe3a476e
jyhgyh <rinat.hirfanov@epitech.eu>
2024-08-05 01:32:14 +0300

    fead: login page connected to back

--- front/src/app/components/login-components/login.component.ts
+++ front/src/app/components/login-components/login.component.ts
@@ -1,32 +1,59 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AuthService } from './../../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  imports: [ReactiveFormsModule, CommonModule]
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  form: FormGroup = new FormGroup({});
-  submitted = false;
+  form: FormGroup;
+  successMessage: string = '';
+  errorMessage: string = '';
+  formSubmitted: boolean = false; 
   showPassword = false;
 
-  constructor(){
-  }
-  
-  ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
-    })
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  ngOnInit() {}
+
   onSubmit() {
-    this.submitted = true;
+    console.log('Form submitted', this.form.value);
+    this.formSubmitted = true;
+
+    if (this.form.valid) {
+      const formData = { ...this.form.value };
+
+      console.log('Info', formData);
+
+      this.authService.login(formData).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          this.successMessage = 'Connexion réussie !';
+          this.errorMessage = '';
+          setTimeout(() => this.router.navigate(['/home']), 3000);
+        },
+        error: (error) => {
+          console.error('Login error', error);
+          this.errorMessage = 'Erreur lors de la connexion. Veuillez réessayer.';
+          this.successMessage = '';
+        }
+      });
+    } else {
+      this.logFormErrors();
+    }
   }
 
   togglePasswordVisibility() {
@@ -38,6 +65,13 @@ export class LoginComponent implements OnInit {
       passwordField.type = 'password';
     }
   }
- }
 
-
+  logFormErrors() {
+    Object.keys(this.form.controls).forEach(controlName => {
+      const control = this.form.get(controlName);
+      if (control?.invalid) {
+        console.log(`Control ${controlName} has errors:`, control.errors);
+      }
+    });
+  }
+}
