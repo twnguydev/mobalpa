8ca22684f82b6a9c271cf7e36390dd41b299bb92
Fabien ivaldi <fabien.ivaldi@epitech.eu>
2024-08-06 16:33:18 +0200

    feat:wishlist page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { CartPageComponent } from './pages/cart_page/cart-page.component';
+import { Wishlist_PageComponent } from './pages/wishlist-page/wishlist-page.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,11 @@ export const routes: Routes = [
     {
         path: 'panier',
         component: CartPageComponent,
+    },
+
+     {
+        path: 'liste-de-souhaits',
+         component: Wishlist_PageComponent,
     }
 ];
 @NgModule({
