18e4fa69b9298563e8584126b00aebc7faf97296
Tanguy Gibrat <gibratanguy@icloud.com>
2024-08-06 14:32:34 +0200

    fix(test): virgule #14

--- api/src/main/java/com/mobalpa/api/config/SecurityConfig.java
+++ api/src/main/java/com/mobalpa/api/config/SecurityConfig.java
@@ -26,9 +26,8 @@ public class SecurityConfig {
             "/api/users/login",
             "/api/users/forgot-password",
             "/api/users/reset-password",
-            "/api/emails/send"
+            "/api/emails/send",
             "/api/orders"
-
     };
 
     @Autowired
