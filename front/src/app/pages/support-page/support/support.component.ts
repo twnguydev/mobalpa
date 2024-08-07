5259d4d5311728706f446c687b01c43f42fc2f38
Fabien ivaldi <fabien.ivaldi@epitech.eu>
2024-08-07 17:28:41 +0200

    update:feedback

--- front/src/app/pages/support-page/support/support.component.ts
+++ front/src/app/pages/support-page/support/support.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './support.component.html',
   styleUrl: './support.component.css'
 })
@@ -14,10 +14,24 @@ export class SupportComponent {
     { title: 'Support' },
     { title: 'Votre avis' }
   ];
+  ratings: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  feedback = {
+    rating: 0,
+    comment: ''
+  };
+
+
 
   selectedTab: number = 0;
 
   selectTab(index: number) {
     this.selectedTab = index;
+  }
+
+
+  onSubmit() {
+    console.log('feedback send');
+
+    //liaison back end
   }
 }
