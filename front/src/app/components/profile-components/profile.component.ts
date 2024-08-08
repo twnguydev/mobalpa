36aa1aebab11b435d1b8300519ef80f39554d3c0
jyhgyh <rinat.hirfanov@epitech.eu>
2024-08-08 17:37:02 +0300

    feat: profile page without .ts

--- front/src/app/components/profile-components/profile.component.ts
+++ front/src/app/components/profile-components/profile.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  tabs = [
+    { title: 'Données personnelles' },
+    { title: 'Modes de paiement' },
+    { title: 'Adresses de livraison' },
+    { title: 'Commandes' }
+  ];
+  ratings: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  feedback = {
+    rating: 0,
+    comment: ''
+  };
 
+
+
+  selectedTab: number = 0;
+
+  selectTab(index: number) {
+    this.selectedTab = index;
+  }
+
+
+  onSubmit() {
+
+  }
 }
