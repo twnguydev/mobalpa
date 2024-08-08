b86cb1c492fab94e6623934e3896c848983c83f6
jyhgyh <rinat.hirfanov@epitech.eu>
2024-08-08 17:53:11 +0300

    feat: profile page without .ts #25

--- front/src/app/components/product-components/product.component.ts
+++ front/src/app/components/product-components/product.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  // form: FormGroup
+  tabs = [
+    { title: 'Tout sur le produit' },
+    { title: 'Caractéristiques' },
+    { title: 'Avis' },
+    { title: 'Caractéristiques' }
+  ];
 
+  selectedTab: number = 0;
+
+  selectTab(index: number) {
+    this.selectedTab = index;
+  }
+
+
+  onSubmit() {
+  }
 }
