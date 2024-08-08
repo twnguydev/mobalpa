61a9bfc509d38ff2466183facca1a8d13071bcf4
NathanMatounga <nathan.matounga@epitech.eu>
2024-08-08 10:22:26 +0200

    Résoudre les conflits de fusion

---
+++ front/src/services/newsletter.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsletterService {
+  private apiUrl: string = `${environment.apiUrl}/emails/newsletter`;
+
+  constructor(private http: HttpClient) {}
+
+  addNewsletter(data: { emailUser: string }): Observable<string> {
+    return this.http.post<string>(this.apiUrl, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': `${environment.apiKey}`
+      }
+    });
+  }
+}
