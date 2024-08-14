f262b6f3c8c895125d4958ada57710603af8d50c
Tanguy Gibrat <gibratanguy@icloud.com>
2024-08-14 12:51:07 +0200

    fix(front): responsive on categories component

--- front/src/app/pages/landing-page/component/show-categorie-on-page/show-categorie-on-page.component.ts
+++ front/src/app/pages/landing-page/component/show-categorie-on-page/show-categorie-on-page.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { ProductService } from '@services/product.service';
+import { ICategory } from '@interfaces/category.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-show-categorie-on-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './show-categorie-on-page.component.html',
   styleUrl: './show-categorie-on-page.component.css'
 })
 export class ShowCategorieOnPageComponent {
+  categories: ICategory[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    this.productService.getCategories().subscribe({
+      next: (response) => {
+        console.log('Categories loaded', response);
+        this.categories = response;
+      },
+      error: (error) => {
+        console.error('Failed to load categories', error);
+      }
+    });
+  }
 }
