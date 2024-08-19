c80cb7ffae70f201d75aaf715eb199cf68cb140b
Tanguy Gibrat <gibratanguy@icloud.com>
2024-08-19 14:27:29 +0200

    fix(front): protect routes for authenticated users

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AuthGuard } from './auth.guard';
+import { NoAuthGuard } from './no-auth.guard';
+
 import { LandingPageComponent } from '@pages/landing-page/landing-page.component';
 import { CartPageComponent } from '@pages/cart_page/cart-page.component';
 import { Wishlist_PageComponent } from '@pages/wishlist-page/wishlist-page.component';
@@ -9,9 +12,8 @@ import { SubcategoriePageComponent } from '@pages/subcategorie-page/subcategorie
 import { HomeComponent } from '@components/home-components/home.component';
 import { LoginComponent } from '@components/login-components/login.component';
 import { RegisterComponent } from '@components/register-components/register.component';
-import { CategoryComponent } from '@components/category-components/category.component';
 import { ResetPasswordComponent } from '@components/reset-password/reset-password.component';
-import { ProductComponent } from '@components/product-components/product.component';
+import { CategoryComponent } from '@components/category-components/category.component';
 import { ProfileComponent } from '@components/profile-components/profile.component';
 import { LegalNoticeComponent } from '@pages/legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from '@pages/privacy-policy/privacy-policy.component';
@@ -20,22 +22,21 @@ import { TermsOfUseComponent } from '@pages/terms-of-use/terms-of-use.component'
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
-    { path: 'panier', component: CartPageComponent },
-    { path: 'liste-de-souhaits', component: Wishlist_PageComponent },
+    { path: 'panier', component: CartPageComponent, canActivate: [AuthGuard] },
+    { path: 'liste-de-souhaits', component: Wishlist_PageComponent, canActivate: [AuthGuard] },
     { path: 'support', component: SupportPageComponent },
     { path: 'info', component: HomeComponent },
-    { path: 'auth/connexion', component: LoginComponent },
-    { path: 'auth/inscription', component: RegisterComponent },
-    { path: 'auth/reinitialiser-mot-de-passe', component: ResetPasswordComponent },
+    { path: 'auth/connexion', component: LoginComponent, canActivate: [NoAuthGuard] },
+    { path: 'auth/inscription', component: RegisterComponent, canActivate: [NoAuthGuard] },
+    { path: 'auth/reinitialiser-mot-de-passe', component: ResetPasswordComponent, canActivate: [NoAuthGuard] },
     { path: 'categories/:categoryUri', component: SubcategoriePageComponent },
     { path: 'categories/:categoryUri/:subcategoryUri', component: CategoryComponent },
-    { path: 'profil', component: ProfileComponent },
+    { path: 'profil', component: ProfileComponent, canActivate: [AuthGuard] },
 
     { path: 'mentions-legales', component: LegalNoticeComponent },
     { path: 'politique-de-confidentialite', component: PrivacyPolicyComponent },
     { path: 'conditions-generales-de-vente', component: TermsAndConditionsComponent },
     { path: 'conditions-generales-utilisation', component: TermsOfUseComponent },
-
 ];
 
 const routerOptions: ExtraOptions = {
