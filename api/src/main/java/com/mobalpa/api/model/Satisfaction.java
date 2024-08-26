07c2d09eccd2562c284489ae28d7483b31daf6b8
Saskoue <romain.farinacci@hotmail.com>
2024-08-26 14:50:10 +0200

    feat: satisfaction entity

---
+++ api/src/main/java/com/mobalpa/api/model/Satisfaction.java
@@ -0,0 +1,43 @@
+package com.mobalpa.api.model;
+
+import lombok.Data;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Satisfaction")
+@Data
+@NoArgsConstructor
+public class Satisfaction {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    @OneToOne
+    @JoinColumn(name = "user_uuid", nullable = true)
+    private User userUuid;
+
+    @Column(name = "target_uuid", nullable = false)
+    private UUID targetUuid;
+
+    @Column(name = "target_type", nullable = false)
+    private String targetType;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "comment", length = 500)
+    private String comment;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime created_at;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created_at = LocalDateTime.now();
+    }
+
+}
