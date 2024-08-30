dc7d295330da674b2d2e80eb236fcf72199ec0e6
Tanguy Gibrat <gibratanguy@icloud.com>
2024-08-30 14:58:49 +0200

    test(data): add unit tests

--- data/start.sh
+++ data/start.sh
@@ -4,7 +4,7 @@ source venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
 pip install --upgrade flask werkzeug
 # pip list
-# python -m unittest discover -s unit_tests
+python -m unittest discover -s unit_tests
 python main.py &
 python api.py &
 wait
--- data/unit_tests/test_main.py
+++ data/unit_tests/test_main.py
@@ -7,7 +7,6 @@ import time
 import sys
 import os
 
-# Ajouter le répertoire parent au sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import main
@@ -19,17 +18,26 @@ class TestMain(unittest.TestCase):
     @patch('main.CSVGenerator')
     @patch('main.EmailSender')
     def test_automate_sales_report_weekly(self, MockEmailSender, MockCSVGenerator, MockSalesAnalyzer, MockDataExtractor):
-        # Mock the data extractor
         mock_extractor = MockDataExtractor.return_value
-        mock_extractor.extract_sales_data.return_value = pd.DataFrame({
+        mock_extractor.extract_sales_data.return_value = (
+            pd.DataFrame({
+                'date': pd.to_datetime(['2023-01-01']),
+                'totalTtc': [120.0]
+            }),
+            pd.DataFrame()
+        )
+        mock_extractor.extract_admin_users.return_value = ['admin@example.com']
+
+        mock_analyzer = MockSalesAnalyzer.return_value
+        mock_analyzer.aggregate_sales.return_value = pd.DataFrame({
             'date': pd.to_datetime(['2023-01-01']),
             'totalTtc': [120.0]
         })
-        mock_extractor.extract_admin_users.return_value = ['admin@example.com']
-        
-        # Mock the sales analyzer
-        mock_analyzer = MockSalesAnalyzer.return_value
-        mock_analyzer.aggregate_sales.return_value = pd.DataFrame({
+        mock_analyzer.create_summary_report.return_value = pd.DataFrame({
+            'Date de début': [pd.to_datetime('2023-01-01')],
+            'Date de fin': [pd.to_datetime('2023-01-07')]
+        })
+        mock_analyzer.create_sales_report.return_value = pd.DataFrame({
             'date': pd.to_datetime(['2023-01-01']),
             'totalTtc': [120.0]
         })
@@ -37,40 +45,36 @@ class TestMain(unittest.TestCase):
             'date': pd.to_datetime(['2023-01-08']),
             'totalTtc': [130.0]
         })
-        
-        # Call the function
+        mock_analyzer.create_predictions_report.return_value = pd.DataFrame({
+            'date': pd.to_datetime(['2023-01-08']),
+            'totalTtc': [130.0]
+        })
+
         main.automate_sales_report(report_type='weekly')
-        
-        # Assertions
+
         MockCSVGenerator.assert_called_once()
         MockEmailSender.assert_called_once()
         mock_extractor.extract_sales_data.assert_called_once()
         mock_extractor.extract_admin_users.assert_called_once()
         mock_analyzer.aggregate_sales.assert_called_once_with(freq='W')
-        mock_analyzer.predict_sales.assert_called_once_with(2, freq='W')
+        mock_analyzer.predict_sales.assert_called_once()
     
     @patch('main.datetime')
     @patch('main.automate_sales_report')
     def test_check_and_run_monthly_task(self, mock_automate_sales_report, mock_datetime):
-        # Mock datetime to return the 30th day at 18:00
         mock_datetime.now.return_value = datetime(2023, 1, 30, 18, 0)
-        
-        # Call the function
+
         main.check_and_run_monthly_task()
-        
-        # Assertions
+
         mock_automate_sales_report.assert_called_once_with(report_type='monthly')
 
     @patch('main.datetime')
     @patch('main.automate_sales_report')
     def test_check_and_run_yearly_task(self, mock_automate_sales_report, mock_datetime):
-        # Mock datetime to return January 30th at 18:00
-        mock_datetime.now.return_value = datetime(2023, 1, 30, 18, 0)
-        
-        # Call the function
+        mock_datetime.now.return_value = datetime(2023, 12, 31, 18, 0)
+
         main.check_and_run_yearly_task()
-        
-        # Assertions
+
         mock_automate_sales_report.assert_called_once_with(report_type='yearly')
     
     @patch('main.schedule.run_pending')
@@ -78,13 +82,18 @@ class TestMain(unittest.TestCase):
     @patch('main.check_and_run_yearly_task')
     @patch('main.time.sleep', return_value=None)
     def test_main_loop(self, mock_sleep, mock_check_and_run_yearly_task, mock_check_and_run_monthly_task, mock_run_pending):
-        # Run the loop for a few iterations
-        main.main_loop(iterations=3)
-        
-        # Assertions
-        mock_run_pending.assert_called()
-        mock_check_and_run_monthly_task.assert_called()
-        mock_check_and_run_yearly_task.assert_called()
+        mock_check_and_run_monthly_task.reset_mock()
+        mock_check_and_run_yearly_task.reset_mock()
+
+        schedule.every().day.at("23:42").do(mock_check_and_run_monthly_task)
+        schedule.every().day.at("23:42").do(mock_check_and_run_yearly_task)
+
+        main.main_loop(iterations=1)
+
+        self.assertGreater(mock_run_pending.call_count, 0, "run_pending was not called as expected")
+
+        self.assertTrue(mock_check_and_run_monthly_task.call_count > 0, "check_and_run_monthly_task was not called")
+        self.assertTrue(mock_check_and_run_yearly_task.call_count > 0, "check_and_run_yearly_task was not called")
 
 if __name__ == '__main__':
     unittest.main()
