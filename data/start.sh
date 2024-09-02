a4599ab539a2e3f9c42c1e4225fd55c094acb119
Saskoue <romain.farinacci@hotmail.com>
2024-09-02 11:27:51 +0200

    feat: added api data to docker

--- data/start.sh
+++ data/start.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-source venv/bin/activate
-pip install --no-cache-dir -r requirements.txt
-pip install --upgrade flask werkzeug
-# pip list
-python -m unittest discover -s unit_tests
-python main.py &
-python api.py &
-wait
+#!/bin/sh
+cd /app
+python3 main.py
